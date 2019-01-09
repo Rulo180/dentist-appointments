60cc5c8af0dc0222e07fd181acaaedd2552cdc20
martinvalles-eb <martinvalles@eventbrite.com>
2019-01-09 12:38:36 -0300

    Initial Commit - Create Meteor project. - Create initial folder structure. - Import React, ReactDOM and bootstrap dependencies. - Add initial boilerplate for the React App.

---
+++ imports/ui/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return (
+            <div className="container">
+                <h1>Welcome to Dentist Appointments!</h1>
+            </div>
+        );
+    }
+}
+ 
+export default App;
