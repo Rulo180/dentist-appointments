51ddc755c08fc054b27250ed9ccaf4ff0d4ff648
martinvalles-eb <martinvalles@eventbrite.com>
2019-02-08 17:26:29 -0300

    Patient Delete Confirmation Modal - Add react-modal dependency. - Create ModalWrapper and DeleteModal components. - Add DeleteModal to PatientsListContainer. - Add modal translations.

--- imports/ui/components/Layout.js
+++ imports/ui/components/Layout.js
@@ -10,9 +10,9 @@ const Layout = ({ children, history }) => {
 	return (
 		<div className="layout">
 			<Navbar history={history}/>
-			<div className="layout__content container pt-3 pb-3">
+			<main className="layout__content container pt-3 pb-3">
 				{children}
-			</div>
+			</main>
 		</div>
 	);
 };
--- imports/ui/container/PatientsListContainer.js
+++ imports/ui/container/PatientsListContainer.js
@@ -1,32 +1,62 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import { Meteor } from 'meteor/meteor';
 import { withTracker } from 'meteor/react-meteor-data';
 import Patients from '../../api/patients';
 
 import PatientsTable from '../components/PatientsTable';
+import DeleteModal from '../components/DeleteModal';
 
+class PatientsListContainer extends PureComponent {
+	constructor(props) {
+		super(props);
+		this.state = {
+			showConfirmationModal: false,
+			modalPatientId: '',
+		};
+	}
 
-const PatientsListContainer = ({ patients, isLoading }) => {
-	
-	const _handleDelete = (id) => {
-		Meteor.call('patient.remove', { _id: id });
+	_handleDelete = () => {
+		Meteor.call('patient.remove', { _id: this.state.modalPatientId });
+		this._closeConfirmationModal();
 	};
-	
-	if(isLoading) {
-		return (
-			<div className="d-flex justify-content-center mt-5">
-				<div className="spinner-border text-primary" role="status">
-					<span className="sr-only">Loading...</span>
+
+	_showConfirmationModal = (id) => {
+		this.setState({ showConfirmationModal: true, modalPatientId: id });
+	}
+
+	_closeConfirmationModal = () => {
+		this.setState({ showConfirmationModal: false });
+	}
+
+	render() { 
+		const { patients, isLoading } = this.props;
+		const { showConfirmationModal, modalPatientId } = this.state;
+
+		if(isLoading) {
+			return (
+				<div className="d-flex justify-content-center mt-5">
+					<div className="spinner-border text-primary" role="status">
+						<span className="sr-only">Loading...</span>
+					</div>
 				</div>
-			</div>
+			);
+		}
+
+		return (
+			<section>
+				<PatientsTable patients={patients} onDelete={this._showConfirmationModal} />
+				<DeleteModal 
+					isOpen={showConfirmationModal}
+					title="Delete confirmation"
+					object={(modalPatientId)?patients.filter((patient) => patient._id === modalPatientId)[0].name:''}
+					onConfirm={this._handleDelete}
+					onClose={this._closeConfirmationModal}
+				/>
+			</section>
 		);
 	}
-	
-	return (
-		<PatientsTable patients={patients} onDelete={_handleDelete} />
-	);
-};
-
+}
+ 
 export default withTracker(() => {
 	const patientsHandler = Meteor.subscribe('patients');
 	const isLoading = !patientsHandler.ready();
